a3a93d843fc50bfe01e316053451bebcf0118e51
tarekmansour <tarekmansour.net@gmail.com>
2025-10-05 18:26:39 +0200

    use ollama

--- src/CSharpMeetsAI/CSharpMeetsAI.AppHost/AppHost.cs
+++ src/CSharpMeetsAI/CSharpMeetsAI.AppHost/AppHost.cs
@@ -1,3 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var ollama = builder.AddOllama("ollama")
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent)
+    ;
+
+var ollamaModel = ollama.AddModel("ollama-model", "phi4")
+    ;
+
 builder.Build().Run();
